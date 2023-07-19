617753e5f50897fff5c98236c95efba5ea319826
uyenvt <uyenvt@rikkeisoft.com>
2023-07-19 15:52:48 +0700

    base

---
+++ VietkaraFE/src/main.js
@@ -0,0 +1,27 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+// Vuetify のインポート
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+// Vuetifyの定義
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+app.use(vuetify);
+
+app.mount('#app')
---
+++ VietkaraFE/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about pagいいいe</h1>
+    <v-checkbox label="Checkbox"></v-checkbox>
+    <v-combobox
+  label="Combobox"
+  :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+></v-combobox>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
